0e682f880c5a61999966fe89e7e765ea8571c681
miracle <josephmiracle119@gmail.com>
2024-01-05 15:33:39 +0100

    changes to wsgi file

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE'))
 
 application = get_wsgi_application()
