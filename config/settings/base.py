fa687db05f672d157a56ae6dd92726a2c702acac
Joseph Miracle <101978290+JoseMiracle@users.noreply.github.com>
2024-10-07 08:34:46 +0100

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 import os, json
-load_dotenv()
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
